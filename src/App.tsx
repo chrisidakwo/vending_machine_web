a14b7334d196a8f95010d88bd1eb74c5bbc52a11
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2022-10-25 00:11:55 +0100

    Remove redux. No need.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { ConnectedRouter } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
 import { UIDReset } from 'react-uid';
 import { renderRoutes } from 'react-router-config';
 import routeConfig from './app/router/routeConfig';
-import { Provider } from 'react-redux';
-import createAppStore from './app/redux/createAppStore';
 import { BrowserRouter } from 'react-router-dom';
 
 const App = (): JSX.Element => {
-  const history = createBrowserHistory();
-  // const store = createAppStore({
-  //   history,
-  // });
-
   return (
     <BrowserRouter>
       <UIDReset>
